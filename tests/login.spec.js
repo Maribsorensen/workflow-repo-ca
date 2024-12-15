e374ad39e6784d2822b95dd40e38a76603716e65
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2024-12-15 19:22:51 +0100

    Feat: added playwright test for navigation. Also added comments on playwright tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,32 +4,32 @@ test.describe("login", () => {
   test("user can successfully log in with valid credentials", async ({
     page,
   }) => {
-    await page.goto("/login");
+    await page.goto("/login"); // Navigates to login page
 
-    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL);
+    await page.locator('input[name="email"]').fill(process.env.TEST_USER_EMAIL); // Finds and fills the email field in the login form with saved env email
     await page
       .locator('input[name="password"]')
-      .fill(process.env.TEST_USER_PASSWORD);
+      .fill(process.env.TEST_USER_PASSWORD); // Finds and fills the password field in the login form with saved env password
 
-    await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("button", { name: "Login" }).click(); // Clicks login button
 
-    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Logout" })).toBeVisible(); // Finds the logout button to check that the login was successful
   });
 
   test("user sees an error message when using invalid credentials", async ({
     page,
   }) => {
-    await page.goto("/login");
+    await page.goto("/login"); //Navigates to login page
 
-    await page.locator('input[name="email"]').fill("wrongemail@gmail.com");
+    await page.locator('input[name="email"]').fill("wrongemail@gmail.com"); // Finds and fills the email field in the login form with a wrong email
     await page
       .locator('input[name="password"]')
-      .fill(process.env.TEST_USER_PASSWORD);
+      .fill(process.env.TEST_USER_PASSWORD); // Finds and fills the password field in the login form with saved env password
 
-    await page.getByRole("button", { name: "Login" }).click();
+    await page.getByRole("button", { name: "Login" }).click(); // Clicks login button
 
     await expect(page.locator("#message-container")).toContainText(
       "Please enter a noroff.no or stud.noroff.no email address.",
-    );
+    ); // Checks the page for a wrong email indicator
   });
 });
